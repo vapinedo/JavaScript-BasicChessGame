4505a994e630945aff5c7003fb044bfa75d2ba17
Victor Pinedo <victor.pineda@linea.ai>
2023-10-14 18:19:44 -0500

    add color schemma to gameboard

--- app.js
+++ app.js
@@ -15,10 +15,18 @@ const startPieces = [
 ];
 
 function createBoard() {
-  startPieces.forEach((startPiece) => {
+  startPieces.forEach((startPiece, index) => {
     const square = document.createElement("div");
-    square.classList.add("square");
-    square.classList.add("beige");
+    square.classList.add("square"); 
+    square.innerHTML = startPiece;
+    square.setAttribute("square-id", index);
+    // square.classList.add("beige");
+    const row =  Math.floor((63 - index) / 8) + 1;
+    if (row % 2 === 0) {
+      square.classList.add(index % 2 === 0 ? "beige": "brown");
+    } else {
+      square.classList.add(index % 2 === 0 ? "brown": "beige");
+    }
     gameBoard.append(square);
   });
 }
