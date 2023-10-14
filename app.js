cff8a797ebd3609080c4972f94a1fb880c145c22
Victor Pinedo <victor.pineda@linea.ai>
2023-10-14 18:00:31 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const gameBoard = document.querySelector("#gameboard");
+const playerDisplay = document.querySelector("#player");
+const infoDisplay = document.querySelector("#info-display");
+const width = 8;
+
+const startPieces = [
+  rook, knight, bishop, queen, king,bishop, knight, rook,
+  pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  '', '', '', '', '', '', '', '',
+  rook, knight, bishop, queen, king,bishop, knight, rook,
+  pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn  
+];
+
+function createBoard() {
+  startPieces.forEach((startPiece) => {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.classList.add("beige");
+    gameBoard.append(square);
+  });
+}
+
+createBoard();
